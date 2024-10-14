c4ee19ca53e372b21855641a58c16e7699a12f68
MeoBeoSiTInh <daocuong.work@gmail.com>
2024-10-14 08:24:45 +0700

    get permission by role (untested)

---
+++ telelink-backend-sails/api/controllers/permission/get.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+
+  friendlyName: 'Get',
+
+
+  description: 'Get permission.',
+
+
+  inputs: {
+    role_id: { type: "number", required: true }
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+    let { req, res, next } = this;
+    const { role_id } = inputs;
+    const result = Permission.findOne({role_id}).populate("action").populate("module")
+    return res.ok(result);
+
+  }
+
+
+};
