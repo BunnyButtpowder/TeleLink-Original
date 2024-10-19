da5f6d4e3754d6c39bae02a8dafd1b2d628dc631
MeoBeoSiTInh <daocuong.work@gmail.com>
2024-10-19 09:01:25 +0700

    "goi cuoc" model

--- telelink-backend-sails/config/bootstrap.js
+++ telelink-backend-sails/config/bootstrap.js
@@ -17,22 +17,38 @@ module.exports.bootstrap = async function () {
     excludeDirs: /^\.(git|svn)$/,
   });
 
-  if(await Auth.count() == 0){
+  if ((await Auth.count()) == 0) {
     await Auth.createEach([
-      {email: 'meobeo@gmail.com', username: 'meobeo', password: '$2b$10$V5BWlTlI7NxV/38PLy3RweHe78ySzZFGuGcBpGzpBkQz7bTu/Ul0q', isActive: 1, role: 1}
-    ])
+      {
+        email: "meobeo@gmail.com",
+        username: "meobeo",
+        password:
+          "$2b$10$V5BWlTlI7NxV/38PLy3RweHe78ySzZFGuGcBpGzpBkQz7bTu/Ul0q",
+        isActive: 1,
+        role: 1,
+      },
+    ]);
   }
 
-  if(await Permission.count() == 0 ){
+  if ((await Permission.count()) == 0) {
     await Permission.createEach([
-      {title: 'TestPermission', path: 'auth/test-permission', method: "GET"}
-    ])
+      { title: "TestPermission", path: "auth/test-permission", method: "GET" },
+    ]);
   }
 
-  if(await Role.count() == 0 ){
+  if ((await Role.count()) == 0) {
     await Role.createEach([
-      {title: 'Admin', onlyViewCreateBy: false, permissions: [1]}
-    ])
+      { title: "Admin", onlyViewCreateBy: false, permissions: [1] },
+      { title: "Salesman", onlyViewCreateBy: false, permissions: [1] },
+      { title: "Agency", onlyViewCreateBy: true, permissions: [1] },
+    ]);
+  }
+
+  if ((await Package.count()) == 0) {
+    await Package.createEach([
+      { code: "20",title: "V160N",provider: "Viettel",type: "Trả trước",price: "160000" },
+      { code: "21",title: "V250N",provider: "Viettel",type: "Trả trước",price: "250000" },
+    ]);
   }
 
   // By convention, this is a good place to set up fake data during development.
