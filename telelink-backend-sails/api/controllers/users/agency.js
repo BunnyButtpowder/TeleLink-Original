b0935bbc110e16f8c0427c2a45041b566182c79f
phucne123111 <128468129+Minhphuc2603@users.noreply.github.com>
2024-10-29 19:06:41 +0700

    create salesman + get salesman

---
+++ telelink-backend-sails/api/controllers/users/agency.js
@@ -0,0 +1,39 @@
+module.exports = {
+  inputs: {
+      agencyId: { type: 'number', required: true },
+  },
+
+  fn: async function (inputs) {
+      let { req, res } = this;
+      try {
+          const { agencyId } = inputs;
+
+
+          const existingAgency = await Agency.findOne({ id: agencyId });
+          if (!existingAgency) {
+              return res.notFound({ message: "Chi nhánh không tồn tại." });
+          }
+          const employees = await User.find({
+              agency: agencyId
+          }).populate('auth');
+          const staffMembers = employees
+                .filter(user => user.auth && user.auth.role === 3)
+                .map(user => ({
+                    id: user.id, 
+                    fullName: user.fullName, 
+                    phoneNumber: user.phoneNumber, 
+                    address: user.address, 
+                    dob: user.dob, 
+                    avatar: user.avatar, 
+                    gender: user.gender, 
+                    email: user.auth.email, 
+                    role: user.auth.role 
+                }));
+
+            return res.json({ employees: staffMembers });
+      } catch (err) {
+          
+          return res.serverError({ error: 'Có lỗi xảy ra khi lấy danh sách nhân viên.',err });
+      }
+  },
+};
