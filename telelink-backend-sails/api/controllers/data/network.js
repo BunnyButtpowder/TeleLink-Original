600e5ed3b8014ab76e0fa6ebf862f2ca6deefb55
phucne123111 <128468129+Minhphuc2603@users.noreply.github.com>
2024-10-31 13:49:51 +0700

    category-agency

---
+++ telelink-backend-sails/api/controllers/data/network.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+
+module.exports = {
+  friendlyName: 'Get all network and categorize with counts',
+
+  description: 'Lấy tất cả dữ liệu, phân loại theo nhà mạng và đếm số lượng trong mỗi loại.',
+
+  fn: async function (inputs) {
+    const { res } = this;
+
+    try {
+      const allData = await Data.find({
+        agency: null
+      });
+      const categorizedData = _.groupBy(allData, 'networkName');
+
+      const categorizedWithCounts = _.mapValues(categorizedData, (items) => ({
+        count: items.length,
+        // items,
+      }));
+
+      return res.status(200).json(categorizedWithCounts);
+    } catch (error) {
+      return res.status(500).json({ message: 'Lỗi khi lấy dữ liệu.', error });
+    }
+  },
+};
