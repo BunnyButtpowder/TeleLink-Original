78e0285cd175299215abd5cd1873a552aa854621
MeoBeoSiTInh <daocuong.work@gmail.com>
2024-11-18 13:51:29 +0700

    Create work.js

---
+++ telelink-backend-sails/api/controllers/rehandle/work.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Work',
+
+
+  description: 'Work rehandle.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
