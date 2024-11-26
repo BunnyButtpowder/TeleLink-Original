2fccca933831b9a7bb9ec8ab02fcd52ad0db7768
phucne123111 <128468129+Minhphuc2603@users.noreply.github.com>
2024-11-26 14:40:44 +0700

    phaan trang fix

--- telelink-backend-sails/api/controllers/data-assign/agency.js
+++ telelink-backend-sails/api/controllers/data-assign/agency.js
@@ -19,30 +19,25 @@ module.exports = {
       description: 'Số lượng data muốn phân bổ cho chi nhánh',
     },
     network: {
-      type: 'string', 
+      type: 'string',
       required: true
     },
     category: {
-      type: 'string', 
+      type: 'string',
       required: true
     },
   },
 
   fn: async function (inputs) {
-    const { agencyId, quantity,network,category } = inputs;
-
-    // // Kiểm tra quyền admin
-    // if (!this.req.me.isAdmin) {
-    //   return this.res.forbidden({ message: 'Bạn không có quyền thực hiện hành động này.' });
-    // }
-
-    
-    const branch = await Agency.findOne({ id: agencyId });
+    const { agencyId, quantity, network, category } = inputs;
+    const branch = await Agency.findOne({ id: agencyId }).populate('users');
+    const userId = branch.users[0].id;
+    console.log(userId);
     if (!branch) {
       return this.res.notFound({ message: 'Chi nhánh không tồn tại.' });
     }
 
-    const availableData = await Data.find({ agency: null,networkName:network ,category:category });
+    const availableData = await Data.find({ agency: null, networkName: network, category: category });
     if (availableData.length === 0) {
       return this.res.notFound({ message: 'Không có data nào sẵn có.' });
     }
@@ -58,6 +53,18 @@ module.exports = {
     await Promise.all(randomDataToAssign.map(async (data) => {
       await Data.updateOne({ id: data.id }).set({ agency: agencyId });
     }));
+    try {
+      await Notifications.create({
+        user: userId,
+        message: `Đã phân bổ thành công ${quantity} data mới.`,
+        status: 'unread',
+      });
+    } catch (error) {
+      console.log(error);
+      return this.res.serverError({
+        message: 'Có lỗi xảy ra khi tạo thông báo.',
+      });
+    }
     sails.sockets.broadcast(
       agencyId,
       'newDataAssigned',
@@ -65,6 +72,7 @@ module.exports = {
         message: `Đã phân bổ thành công ${quantity} data mới.`,
       }
     );
+    console.log('WebSocket broadcast triggered for room:', agencyId);
 
     return this.res.ok({
       message: `Đã phân bổ thành công ${quantity} data ngẫu nhiên cho chi nhánh ${branch.name}.`,
@@ -72,3 +80,5 @@ module.exports = {
     });
   }
 };
+
+
