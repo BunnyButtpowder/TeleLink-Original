0fcd0fc00918e575d4d7641c3aa8310ead5a132b
ThanhPD2003 <132969411+ThanhPD2003@users.noreply.github.com>
2024-12-28 10:27:00 +0700

    Hotfix #1
    - Add splitter on money in customer and data page.
    - Add date to data
    - Distribute data to multiple users in 1 time

--- telelink-frontend/src/app/pages/customer/customers-list/components/header/CustomersListToolbar.tsx
+++ telelink-frontend/src/app/pages/customer/customers-list/components/header/CustomersListToolbar.tsx
@@ -87,7 +87,6 @@ const CustomersListToolbar = () => {
 
   return (
     <>
-      <ToastContainer />
       <div className='d-flex justify-content-end' data-kt-user-table-toolbar='base'>
         {/* <UsersListFilter /> */}
 
